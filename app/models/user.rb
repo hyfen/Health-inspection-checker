4b1e323312af82d4fb0d2cae84a1eeced6c7e7ae
Andrew Louis <andrew@hyfen.net>
2011-02-25 16:12:47 -0500

    Cache foursquare checkins for a user so don't have to use the API each time for testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+  
+  def cache_checkins
+    
+    client = Foursquare2::Client.new(:oauth_token => self.foursquare_token)
+    self.checkins = Marshal.dump(client.user_checkins)
+    save
+    
+  end
+  
+  def cached_checkins
+    Marshal.load(self.checkins)
+  end
+  
 end
